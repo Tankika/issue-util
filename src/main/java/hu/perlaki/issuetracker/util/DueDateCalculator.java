ae93a33ab582bb0244605a025d781371714b8c1e
Tamás Perlaki <perlaki.tamas@argonsoft.hu>
2017-06-23 13:17:39 +0200

    Fixed indentations

--- src/main/java/hu/perlaki/issuetracker/util/DueDateCalculator.java
+++ src/main/java/hu/perlaki/issuetracker/util/DueDateCalculator.java
@@ -8,98 +8,98 @@ import java.time.temporal.TemporalAdjusters;
 
 public class DueDateCalculator 
 {
-	
-	public static final DayOfWeek WORK_DAYS_START = DayOfWeek.MONDAY;
-	public static final DayOfWeek WORK_DAYS_END = DayOfWeek.FRIDAY;
-	public static final LocalTime WORK_HOURS_START = LocalTime.of(9, 0);
-	public static final LocalTime WORK_HOURS_END = LocalTime.of(17, 0);
-	public static final int WORK_HOURS_LENGTH = Math.toIntExact(ChronoUnit.HOURS.between(WORK_HOURS_START, WORK_HOURS_END));
-	
+    
+    public static final DayOfWeek WORK_DAYS_START = DayOfWeek.MONDAY;
+    public static final DayOfWeek WORK_DAYS_END = DayOfWeek.FRIDAY;
+    public static final LocalTime WORK_HOURS_START = LocalTime.of(9, 0);
+    public static final LocalTime WORK_HOURS_END = LocalTime.of(17, 0);
+    public static final int WORK_HOURS_LENGTH = Math.toIntExact(ChronoUnit.HOURS.between(WORK_HOURS_START, WORK_HOURS_END));
+
     public LocalDateTime CalculateDueDate(LocalDateTime submitDate, int turnaroundTime)
     {
-    	validateInputs(submitDate, turnaroundTime);
-    	
-    	LocalDateTime dueDate = LocalDateTime.from(submitDate);
-    	
-    	dueDate = increaseDateWithWholeDays(dueDate, turnaroundTime);
-    	dueDate = increaseDateWithRemainingTime(dueDate, turnaroundTime);
-    	
+        validateInputs(submitDate, turnaroundTime);
+        
+        LocalDateTime dueDate = LocalDateTime.from(submitDate);
+        
+        dueDate = increaseDateWithWholeDays(dueDate, turnaroundTime);
+        dueDate = increaseDateWithRemainingTime(dueDate, turnaroundTime);
+        
         return dueDate;
     }
 
-	private void validateInputs(LocalDateTime submitDateTime, int turnaroundTime) {
-		validateSubmitDate(submitDateTime);
-		validateTurnaroundTime(turnaroundTime);
-	}
+    private void validateInputs(LocalDateTime submitDateTime, int turnaroundTime) {
+        validateSubmitDate(submitDateTime);
+        validateTurnaroundTime(turnaroundTime);
+    }
 
-	private void validateSubmitDate(LocalDateTime submitDateTime) {
-		DayOfWeek submitDayOfWeek = submitDateTime.getDayOfWeek();
-		LocalTime submitTime = submitDateTime.toLocalTime();
-		
-		if(!isWorkDay(submitDayOfWeek)) {
-			throw new IllegalArgumentException(
-					String.format("Submit date must be on a work day. First work day is: %s, last work day is: %s", WORK_DAYS_START, WORK_DAYS_END));
-		} else if(submitTime.isBefore(WORK_HOURS_START) || submitTime.isAfter(WORK_HOURS_END)) {
-			throw new IllegalArgumentException(
-					String.format("Submit time must be in a working hour. Work hours start: %s, work hours end: %s", WORK_HOURS_START, WORK_HOURS_END));
-		}
-	}
+    private void validateSubmitDate(LocalDateTime submitDateTime) {
+        DayOfWeek submitDayOfWeek = submitDateTime.getDayOfWeek();
+        LocalTime submitTime = submitDateTime.toLocalTime();
+        
+        if(!isWorkDay(submitDayOfWeek)) {
+            throw new IllegalArgumentException(
+                    String.format("Submit date must be on a work day. First work day is: %s, last work day is: %s", WORK_DAYS_START, WORK_DAYS_END));
+        } else if(submitTime.isBefore(WORK_HOURS_START) || submitTime.isAfter(WORK_HOURS_END)) {
+            throw new IllegalArgumentException(
+                    String.format("Submit time must be in a working hour. Work hours start: %s, work hours end: %s", WORK_HOURS_START, WORK_HOURS_END));
+        }
+    }
 
-	private void validateTurnaroundTime(int turnaroundTime) {
-		if(turnaroundTime <= 0) {
-			throw new IllegalArgumentException(
-					String.format("Turnaround time must be positive"));
-		}
-	}
+    private void validateTurnaroundTime(int turnaroundTime) {
+        if(turnaroundTime <= 0) {
+            throw new IllegalArgumentException(
+                    String.format("Turnaround time must be positive"));
+        }
+    }
 
-	private LocalDateTime increaseDateWithWholeDays(LocalDateTime date, int turnaroundTime) {
-		int turnaroundDays = turnaroundTime / WORK_HOURS_LENGTH,
-				turnaroundWeeks = turnaroundDays / 7,
-				remainingDays = turnaroundDays % 7;
-		
-		date = date.plusWeeks(turnaroundWeeks);
-		date = addWorkingDaysToDate(date, remainingDays);
-    	
-		return date;
-	}
+    private LocalDateTime increaseDateWithWholeDays(LocalDateTime date, int turnaroundTime) {
+        int turnaroundDays = turnaroundTime / WORK_HOURS_LENGTH,
+                turnaroundWeeks = turnaroundDays / 7,
+                remainingDays = turnaroundDays % 7;
+        
+        date = date.plusWeeks(turnaroundWeeks);
+        date = addWorkingDaysToDate(date, remainingDays);
+        
+        return date;
+    }
 
-	private LocalDateTime addWorkingDaysToDate(LocalDateTime date, int days) {
-    	for(int i = 0; i < days; i++) {
-    		date = date.plusDays(1);
-    		date = adjustToNextOrSameWorkingDay(date);
-		}
-    	
-    	return date;
-	}
+    private LocalDateTime addWorkingDaysToDate(LocalDateTime date, int days) {
+        for(int i = 0; i < days; i++) {
+            date = date.plusDays(1);
+            date = adjustToNextOrSameWorkingDay(date);
+        }
+        
+        return date;
+    }
 
-	private LocalDateTime increaseDateWithRemainingTime(LocalDateTime date, int turnaroundTime) {
-		int remainingTurnaroundMinutes = turnaroundTime % WORK_HOURS_LENGTH * 60,
-				remainingMinutesOfDay = Math.toIntExact(ChronoUnit.MINUTES.between(date.toLocalTime(), WORK_HOURS_END));
-    	
-		if(remainingTurnaroundMinutes <= remainingMinutesOfDay) {
-    		date = date.plusMinutes(remainingTurnaroundMinutes); 
-    	} else {
-    		int rolloverMinutes = remainingTurnaroundMinutes - remainingMinutesOfDay;
-    		
-    		date = LocalDateTime.of(date.toLocalDate(), WORK_HOURS_START)
-    					.plusDays(1)
-    					.plusMinutes(rolloverMinutes);
-    		date = adjustToNextOrSameWorkingDay(date);
-    	}
-		
-		return date;
-	}
+    private LocalDateTime increaseDateWithRemainingTime(LocalDateTime date, int turnaroundTime) {
+        int remainingTurnaroundMinutes = turnaroundTime % WORK_HOURS_LENGTH * 60,
+                remainingMinutesOfDay = Math.toIntExact(ChronoUnit.MINUTES.between(date.toLocalTime(), WORK_HOURS_END));
+        
+        if(remainingTurnaroundMinutes <= remainingMinutesOfDay) {
+            date = date.plusMinutes(remainingTurnaroundMinutes); 
+        } else {
+            int rolloverMinutes = remainingTurnaroundMinutes - remainingMinutesOfDay;
+            
+            date = LocalDateTime.of(date.toLocalDate(), WORK_HOURS_START)
+                        .plusDays(1)
+                        .plusMinutes(rolloverMinutes);
+            date = adjustToNextOrSameWorkingDay(date);
+        }
+        
+        return date;
+    }
 
-	private LocalDateTime adjustToNextOrSameWorkingDay(LocalDateTime date) {
-		if(!isWorkDay(date.getDayOfWeek())) {
-			date = date.with(TemporalAdjusters.next(WORK_DAYS_START)); 
-		}
-		
-		return date;
-	}
+    private LocalDateTime adjustToNextOrSameWorkingDay(LocalDateTime date) {
+        if(!isWorkDay(date.getDayOfWeek())) {
+            date = date.with(TemporalAdjusters.next(WORK_DAYS_START)); 
+        }
+        
+        return date;
+    }
 
-	private boolean isWorkDay(DayOfWeek dayOfWeek) {
-		return 0 <= dayOfWeek.compareTo(WORK_DAYS_START) && dayOfWeek.compareTo(WORK_DAYS_END) <= 0;
-	}
-	
+    private boolean isWorkDay(DayOfWeek dayOfWeek) {
+        return 0 <= dayOfWeek.compareTo(WORK_DAYS_START) && dayOfWeek.compareTo(WORK_DAYS_END) <= 0;
+    }
+    
 }
--- src/test/java/hu/perlaki/issuetracker/util/DueDateCalculatorTest.java
+++ src/test/java/hu/perlaki/issuetracker/util/DueDateCalculatorTest.java
@@ -10,133 +10,133 @@ import static org.junit.Assert.*;
 public class DueDateCalculatorTest
 {
 
-	private DueDateCalculator dueDateCalculator;
-	
-	@Before
-	public void setUp() {
-		dueDateCalculator = new DueDateCalculator();
-	}
-	
-	@Test
+    private DueDateCalculator dueDateCalculator;
+    
+    @Before
+    public void setUp() {
+        dueDateCalculator = new DueDateCalculator();
+    }
+    
+    @Test
     public void testCalculateDueDate_OnlyWorkDays_WholeDaysTurnaround()
     {
         LocalDateTime submitDateTime = LocalDateTime.of(2017, Month.JUNE, 20, 14, 12), // Tuesday
-        		expectedDateTime = LocalDateTime.of(2017, Month.JUNE, 22, 14, 12);
+                expectedDateTime = LocalDateTime.of(2017, Month.JUNE, 22, 14, 12);
         
         LocalDateTime dueDateTime = dueDateCalculator.CalculateDueDate(submitDateTime, 16);
         
         assertEquals(expectedDateTime, dueDateTime);
     }
-	
-	@Test
-	public void testCalculateDueDate_OnlyWorkDays_PartialDaysTurnaround_WithoutHourRollover()
+    
+    @Test
+    public void testCalculateDueDate_OnlyWorkDays_PartialDaysTurnaround_WithoutHourRollover()
     {
         LocalDateTime submitDateTime = LocalDateTime.of(2017, Month.JUNE, 20, 10, 7), // Tuesday
-        		expectedDateTime = LocalDateTime.of(2017, Month.JUNE, 21, 13, 7);
+                expectedDateTime = LocalDateTime.of(2017, Month.JUNE, 21, 13, 7);
         
         LocalDateTime dueDateTime = dueDateCalculator.CalculateDueDate(submitDateTime, 11);
         
         assertEquals(expectedDateTime, dueDateTime);
     }
-	
-	@Test
-	public void testCalculateDueDate_OnlyWorkDays_PartialDaysTurnaround_WithHourRollover()
+    
+    @Test
+    public void testCalculateDueDate_OnlyWorkDays_PartialDaysTurnaround_WithHourRollover()
     {
         LocalDateTime submitDateTime = LocalDateTime.of(2017, Month.JUNE, 20, 17, 0), // Tuesday
-        		expectedDateTime = LocalDateTime.of(2017, Month.JUNE, 23, 12, 0);
+                expectedDateTime = LocalDateTime.of(2017, Month.JUNE, 23, 12, 0);
         
         LocalDateTime dueDateTime = dueDateCalculator.CalculateDueDate(submitDateTime, 19);
         
         assertEquals(expectedDateTime, dueDateTime);
     }
-	
-	@Test
+    
+    @Test
     public void testCalculateDueDate_WithNonWorkingDays_WholeDaysTurnaround()
     {
         LocalDateTime submitDateTime = LocalDateTime.of(2017, Month.DECEMBER, 29, 9, 0), // Friday
-        		expectedDateTime = LocalDateTime.of(2018, Month.JANUARY, 2, 9, 0);
+                expectedDateTime = LocalDateTime.of(2018, Month.JANUARY, 2, 9, 0);
         
         LocalDateTime dueDateTime = dueDateCalculator.CalculateDueDate(submitDateTime, 16);
         
         assertEquals(expectedDateTime, dueDateTime);
     }
-	
-	@Test
-	public void testCalculateDueDate_WithNonWorkingDays_PartialDays_WithoutHourRollover()
+    
+    @Test
+    public void testCalculateDueDate_WithNonWorkingDays_PartialDays_WithoutHourRollover()
     {
         LocalDateTime submitDateTime = LocalDateTime.of(2024, Month.FEBRUARY, 28, 10, 35), // Wednesday, in a leap year
-        		expectedDateTime = LocalDateTime.of(2024, Month.MARCH, 4, 11, 35);
+                expectedDateTime = LocalDateTime.of(2024, Month.MARCH, 4, 11, 35);
         
         LocalDateTime dueDateTime = dueDateCalculator.CalculateDueDate(submitDateTime, 25);
         
         assertEquals(expectedDateTime, dueDateTime);
     }
-	
-	@Test
-	public void testCalculateDueDate_WithNonWorkingDays_PartialDays_WithHourRollover()
+    
+    @Test
+    public void testCalculateDueDate_WithNonWorkingDays_PartialDays_WithHourRollover()
     {
         LocalDateTime submitDateTime = LocalDateTime.of(2017, Month.JUNE, 23, 16, 52), // Friday
-        		expectedDateTime = LocalDateTime.of(2017, Month.JUNE, 26, 15, 52);
+                expectedDateTime = LocalDateTime.of(2017, Month.JUNE, 26, 15, 52);
         
         LocalDateTime dueDateTime = dueDateCalculator.CalculateDueDate(submitDateTime, 7);
         
         assertEquals(expectedDateTime, dueDateTime);
     }
-	
-	@Test(expected=IllegalArgumentException.class)
+    
+    @Test(expected=IllegalArgumentException.class)
     public void testCalculateDueDate_IsWorkingDayValidation_OutOfIntervalLowerBoundary()
     {
         LocalDateTime submitDateTime = LocalDateTime.of(2017, Month.JUNE, 24, 10, 0); // Saturday
-		dueDateCalculator.CalculateDueDate(submitDateTime, 1);
+        dueDateCalculator.CalculateDueDate(submitDateTime, 1);
     }
-	
-	@Test(expected=IllegalArgumentException.class)
+    
+    @Test(expected=IllegalArgumentException.class)
     public void testCalculateDueDate_IsWorkingDayValidation_OutOfIntervalHigherBoundary()
     {
         LocalDateTime submitDateTime = LocalDateTime.of(2017, Month.JUNE, 25, 10, 0); // Sunday
-		dueDateCalculator.CalculateDueDate(submitDateTime, 1);
+        dueDateCalculator.CalculateDueDate(submitDateTime, 1);
     }
-	
-	@Test(expected=IllegalArgumentException.class)
+    
+    @Test(expected=IllegalArgumentException.class)
     public void testCalculateDueDate_IsWorkingHourValidation_OutOfIntervalLowerBoundary()
     {
-		dueDateCalculator.CalculateDueDate(LocalDateTime.of(2017, Month.JUNE, 21, 17, 1), 1);
+        dueDateCalculator.CalculateDueDate(LocalDateTime.of(2017, Month.JUNE, 21, 17, 1), 1);
     }
-	
-	@Test(expected=IllegalArgumentException.class)
+    
+    @Test(expected=IllegalArgumentException.class)
     public void testCalculateDueDate_IsWorkingHourValidation_OutOfIntervalHigherBoundary()
     {
-		dueDateCalculator.CalculateDueDate(LocalDateTime.of(2017, Month.JUNE, 21, 8, 59), 1);
+        dueDateCalculator.CalculateDueDate(LocalDateTime.of(2017, Month.JUNE, 21, 8, 59), 1);
     }
-	
-	@Test(expected=IllegalArgumentException.class)
+    
+    @Test(expected=IllegalArgumentException.class)
     public void testCalculateDueDate_IsWorkingHourValidation_OutOfInterval()
     {
-		dueDateCalculator.CalculateDueDate(LocalDateTime.of(2017, Month.JUNE, 21, 23, 59), 1);
+        dueDateCalculator.CalculateDueDate(LocalDateTime.of(2017, Month.JUNE, 21, 23, 59), 1);
     }
-	
-	@Test(expected=IllegalArgumentException.class)
+    
+    @Test(expected=IllegalArgumentException.class)
     public void testCalculateDueDate_TurnaroundTimeValidation_NonPositiveBoundary()
     {
-		dueDateCalculator.CalculateDueDate(LocalDateTime.of(2017, Month.JUNE, 21, 13, 0), -1000);
+        dueDateCalculator.CalculateDueDate(LocalDateTime.of(2017, Month.JUNE, 21, 13, 0), -1000);
     }
-	
-	@Test(expected=IllegalArgumentException.class)
+    
+    @Test(expected=IllegalArgumentException.class)
     public void testCalculateDueDate_TurnaroundTimeValidation_NonPositiveHigherBoundary()
     {
-		dueDateCalculator.CalculateDueDate(LocalDateTime.of(2017, Month.JUNE, 21, 13, 0), -1);
+        dueDateCalculator.CalculateDueDate(LocalDateTime.of(2017, Month.JUNE, 21, 13, 0), -1);
     }
-	
-	@Test(expected=IllegalArgumentException.class)
+    
+    @Test(expected=IllegalArgumentException.class)
     public void testCalculateDueDate_TurnaroundTimeValidation_NonPositiveLowerBoundary()
     {
-		dueDateCalculator.CalculateDueDate(LocalDateTime.of(2017, Month.JUNE, 21, 13, 0), Integer.MIN_VALUE);
+        dueDateCalculator.CalculateDueDate(LocalDateTime.of(2017, Month.JUNE, 21, 13, 0), Integer.MIN_VALUE);
     }
-	
-	@Test(expected=NullPointerException.class)
+    
+    @Test(expected=NullPointerException.class)
     public void testCalculateDueDate_NullDate()
     {
-		dueDateCalculator.CalculateDueDate(null, 2);
+        dueDateCalculator.CalculateDueDate(null, 2);
     }
-	
+    
 }
